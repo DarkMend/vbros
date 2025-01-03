5c37794deb96e77818429102f495a706197b9f95
DarkMend <gindullin20@mail.ru>
2025-01-03 19:52:02 +0300

    sidebar and scss

--- frontend/src/layouts/MainLayout/MainLayout.tsx
+++ frontend/src/layouts/MainLayout/MainLayout.tsx
@@ -1,13 +1,31 @@
 import { Outlet } from 'react-router-dom'
-import styles from './MainLayout.module.css'
+import styles from './MainLayout.module.scss'
 
 export default function MainLayout() {
     return (
         <div className={styles['main-layout']}>
-            <div className="sidebar">
-                
+            <div className={styles['sidebar']}>
+                <div className={styles['logo']}>
+                    <div className={styles['img']}>
+                        <img src="./sqrt.png" alt="" />
+                    </div>
+                    <h2>VBROS</h2>
+                </div>
+                <div className={styles['menu']}>
+                    <a href="">qweq</a>
+                    <a href="">123123</a>
+                    <a href="">qwe</a>
+                </div>
+                <div className={styles['profile__wrapper']}>
+                    <div className={styles['premium']}>
+
+                    </div>
+                    <div className={styles['profile']}>
+
+                    </div>
+                </div>
             </div>
-            <div className="page">
+            <div className={styles['page']}>
                 <Outlet />
             </div>
         </div>
