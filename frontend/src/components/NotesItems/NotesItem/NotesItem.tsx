a64aaebc6f9c22c387b26ffa2e51002668d21c7e
DarkMend <gindullin20@mail.ru>
2025-01-22 11:49:52 +0300

    ne cmog

--- frontend/src/components/NotesItems/NotesItem/NotesItem.tsx
+++ frontend/src/components/NotesItems/NotesItem/NotesItem.tsx
@@ -3,7 +3,6 @@ import "react-loading-skeleton/dist/skeleton.css";
 import styles from "./NotesItem.module.scss";
 import { useDrag } from "react-dnd";
 import cn from "classnames";
-import { useState } from "react";
 
 export default function NotesItem({ data, ...props }: { data: Object }) {
   const [{ isDragStart }, dragRef] = useDrag({
@@ -14,29 +13,13 @@ export default function NotesItem({ data, ...props }: { data: Object }) {
     }),
   });
 
-  const [isMouseDown, setIsMouseDown] = useState(false);
-
-  const handleMouseDown = () => {
-    setIsMouseDown(true);
-    console.log('s')
-  }
-
-  const handleMouseUp = () => {
-    setIsMouseDown(false);
-    console.log('a')
-
-  }
-
   return (
     <>
       <div
         className={cn(styles["notes-item"], {
-          [styles["isDragging"]]: isDragStart,
-          [styles['preview']]: isMouseDown
+          [styles["isDragging"]]: isDragStart
         })}
         ref={dragRef}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
       >
         <div className={styles["title"]}>{data.title}</div>
         <div className={styles["description"]}>
