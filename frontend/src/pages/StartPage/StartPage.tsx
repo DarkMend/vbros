b99e53527e1231dc305672c01270efa01158761c
DarkMend <gindullin20@mail.ru>
2025-02-12 16:35:38 +0300

    button

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -1,17 +1,18 @@
+import MainButton from '../../components/MainButton/MainButton';
 import styles from './StartPage.module.scss';
 export default function StartPage() {
     return (
         <div className={styles['start-page']}>
             <div className={styles['start-page__head']}>
                 <div className="logo">
-                    <img src="./logo.svg" alt="Логтип сайта" />
+                    <img src="/logo.svg" alt="Логтип сайта"/>
                 </div>
                 <h1>
                     <span>Удобный и минималистичный инструмент</span>, помогающий быстро фиксировать задачи, расставлять приоритеты и не терять фокус.
                 </h1>
             </div>
             <div className={styles['start-page__actions']}>
-
+                <MainButton onClick={() => console.log('fff')}>Войти</MainButton>
             </div>
         </div>
     )
