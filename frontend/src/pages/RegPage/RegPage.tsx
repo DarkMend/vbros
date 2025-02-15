82960d6178ce92baa39ebbdb7ecb11d145622823
DarkMend <gindullin20@mail.ru>
2025-02-15 14:25:11 +0300

    form

--- frontend/src/pages/RegPage/RegPage.tsx
+++ frontend/src/pages/RegPage/RegPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import FormLayout from "../../components/FormLayout/FormLayout";
 import MainButton from "../../components/MainButton/MainButton";
 import MainInput from "../../components/MainInput/MainInput";
@@ -12,7 +13,7 @@ export interface IFormReg {
 }
 
 export default function RegPage() {
-  const { register, formState: {errors}, handleSubmit } = useForm<IFormReg>({
+  const { register, formState: { errors }, handleSubmit, watch } = useForm<IFormReg>({
     mode: "onSubmit",
   });
 
@@ -25,7 +26,7 @@ export default function RegPage() {
       <h1>Создайте аккаунт</h1>
       <FormLayout onSubmit={handleSubmit(onSubmit)}>
         <MainInput placeholder="Имя" {...register('name', {
-            required: 'Заполните имя',
+          required: 'Заполните имя',
         })} errorMessage={errors.name?.message} />
         <MainInput placeholder="Почта" {...register('email', {
           required: 'Заполните почту',
@@ -33,17 +34,25 @@ export default function RegPage() {
             value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
             message: 'Введите корректную почту'
           }
-        })} errorMessage={errors.email?.message}/>
-        <MainInput placeholder="Пароль" {...register('password', {
-          required: 'Заполните пароль'
-        })} errorMessage={errors.password?.message}/>
-        <MainInput placeholder="Подтверждение пароля" {...register('password_confirmation', {
-          required: 'Заполните подтверждение пароля'
-        })} errorMessage={errors.password_confirmation?.message}/>
+        })} errorMessage={errors.email?.message} />
+        <MainInput placeholder="Пароль" type="password" {...register('password', {
+          required: 'Заполните пароль',
+          minLength: { value: 6, message: 'В пароле должно быть не менее 6 символов' }
+        })} errorMessage={errors.password?.message} />
+        <MainInput placeholder="Подтверждение пароля" type="password" {...register('password_confirmation', {
+          required: 'Заполните подтверждение пароля',
+          validate: (val: string) => {
+            if (watch('password') != val) {
+              return 'Пароли не совпадают'
+            }
+          }
+        })} errorMessage={errors.password_confirmation?.message} />
       </FormLayout>
       <div className={styles["transition-block"]}>
         <p className={styles["transition-block__text"]}>У вас есть аккаунт?</p>
-        <MainButton theme="white">Войти</MainButton>
+        <Link to={'/auth/login'}>
+          <MainButton theme="white">Войти</MainButton>
+        </Link>
       </div>
     </div>
   );
