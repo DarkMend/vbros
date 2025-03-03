b51a0fcb7fd3aaf47dc4ecb7849690db5f01c2d8
DarkMend <gindullin20@mail.ru>
2025-03-03 12:49:38 +0300

    profile-settings

--- frontend/src/components/Sidebar/Profile/Profile.tsx
+++ frontend/src/components/Sidebar/Profile/Profile.tsx
@@ -1,29 +1,24 @@
-import { useModalStore } from '../../../store/modalStore'
-import ProfileIconButton from '../../ProfileIconButton/ProfileIconButton'
-import styles from './Profile.module.scss'
-import ProfileSettings from './ProfileSettings/ProfileSettings';
+import ProfileIconButton from "../../ProfileIconButton/ProfileIconButton";
+import styles from "./Profile.module.scss";
+import ProfileSettings from "./ProfileSettings/ProfileSettings";
 
 export default function Profile() {
-    // const openModal = useModalStore(state => state.openModal);
-
-    return (
-        <>
-            <div className={styles['profile']}>
-                <div className={styles['profile__user']}>
-                    <div className={styles['ava']}>
-                        <img src="./icons/circle.svg" alt="" />
-                    </div>
-                    <div className={styles['name']}>
-                        Аяз
-                    </div>
-                </div>
-                <div className={styles['arrow']}>
-                    <ProfileIconButton >
-                        <img src="./icons/menu-select.svg" alt="октрыть меню" />
-                    </ProfileIconButton>
-                    <ProfileSettings />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={styles["profile"]}>
+        <div className={styles["profile__user"]}>
+          <div className={styles["ava"]}>
+            <img src="./icons/circle.svg" alt="" />
+          </div>
+          <div className={styles["name"]}>Аяз</div>
+        </div>
+        <div className={styles["arrow"]}>
+          <ProfileIconButton>
+            <img src="./icons/menu-select.svg" alt="октрыть меню" />
+          </ProfileIconButton>
+          <ProfileSettings />
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/components/Sidebar/Profile/ProfileSettings/ProfileSettings.tsx
+++ frontend/src/components/Sidebar/Profile/ProfileSettings/ProfileSettings.tsx
@@ -1,12 +1,25 @@
-import styles from './ProfileSettings.module.scss';
-import ProfileSettingsItem from './ProfileSettingsItem/ProfileSettingsItem';
-import AccountIcon from './../../../../../public/icons/account.svg';
-
+import styles from "./ProfileSettings.module.scss";
+import ProfileSettingsItem from "./ProfileSettingsItem/ProfileSettingsItem";
+import AccountIcon from "./../../../../../public/icons/account.svg";
+import GearIcon from "./../../../../../public/icons/gear.svg";
+import SupportIcon from "./../../../../../public/icons/support.svg";
+import ExitIcon from "./../../../../../public/icons/exit.svg";
 
 export default function ProfileSettings() {
-    return (
-        <div className={styles['profile-settings']}>
-            <ProfileSettingsItem name='Аккаунт' icon={<AccountIcon />} />
-        </div>
-    ) 
+  return (
+    <div className={styles["profile-settings"]}>
+      <div className={styles["profile-settings__wrapper"]}>
+        <ProfileSettingsItem name="Аккаунт" icon={<AccountIcon />} />
+        <ProfileSettingsItem name="Настройки" icon={<GearIcon />} />
+      </div>
+      <div className={styles["hr"]}></div>
+      <div className={styles["profile-settings__wrapper"]}>
+        <ProfileSettingsItem name="Поддержка" icon={<SupportIcon />} />
+      </div>
+      <div className={styles["hr"]}></div>
+      <div className={styles["profile-settings__wrapper"]}>
+        <ProfileSettingsItem name="Выйти" icon={<ExitIcon />} />
+      </div>
+    </div>
+  );
 }
