7b89e9a3a48b18fc12e383d4ddb1b6f475a1c606
DarkMend <gindullin20@mail.ru>
2025-03-05 11:44:02 +0300

    notesItem

--- frontend/src/components/NotesItems/NotesItem/NotesItem.tsx
+++ frontend/src/components/NotesItems/NotesItem/NotesItem.tsx
@@ -1,4 +1,3 @@
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import styles from "./NotesItem.module.scss";
 import cn from "classnames";
@@ -6,28 +5,31 @@ import { useDraggable } from "@dnd-kit/core";
 import { INotesItem } from "./NotesItem.props";
 
 export default function NotesItem({ note }: INotesItem) {
-
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: note.id,
-
   });
 
   const style = {
-    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-    cursor: 'grab',
-  }
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : undefined,
+    cursor: "grab",
+  };
 
   return (
     <>
       <div
-        className={cn(styles["notes-item"])} style={style} ref={ setNodeRef } {...attributes} {...listeners}
+        className={cn(styles["notes-item"])}
+        style={style}
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
       >
         <div className={styles["title"]}>{note.title}</div>
-        <div className={styles["description"]}>
-          <Skeleton height={17} />
-          {note.description}
+        <div className={styles["description"]}>{note.description}</div>
+        <div className={styles["deadline"]}>
+          {new Date(note.created_at).toLocaleDateString()}
         </div>
-        <div className={styles["deadline"]}>{new Date(note.created_at).toLocaleDateString()}</div>
       </div>
     </>
   );
