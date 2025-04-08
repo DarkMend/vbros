dcdd7f39056f1bbcff4d47c466b9d45e930179d3
DarkMend <gindullin20@mail.ru>
2025-04-08 19:56:07 +0300

    datemodal

--- frontend/src/components/Modals/DateModal/DateModal.tsx
+++ frontend/src/components/Modals/DateModal/DateModal.tsx
@@ -17,7 +17,24 @@ export default function DateModal() {
   return (
     <ModalLayout title="Дедлайн" icon={<CalendarIcon />}>
       <div className={styles.dateChange}>
-        <Calendar onClickDay={changeDate} value={date} />
+        <Calendar
+          onClickDay={changeDate}
+          value={date}
+          locale="ru-RU"
+          minDetail="year"
+          next2Label={null}
+          prev2Label={null}
+          tileClassName={({ date: day, view }) => {
+            if (view === "month") {
+              const classes = [];
+              if (day.getDay() === 0 || day.getDay() === 6) {
+                classes.push(styles.weekend);
+              }
+              return classes.join(" ");
+            }
+            return null;
+          }}
+        />
       </div>
     </ModalLayout>
   );
