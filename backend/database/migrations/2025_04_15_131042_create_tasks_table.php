3f47132288d3460f0fd36476dcc6b0fa4bac23f9
DarkMend <gindullin20@mail.ru>
2025-04-22 13:44:55 +0300

    make model and migrations

---
+++ backend/database/migrations/2025_04_15_131042_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->dateTime('сompletion_time');
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('status_project_id')->constrained('status_projects')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
