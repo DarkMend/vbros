fd467937864b4d03a4c4832c56e24e00cb81e7de
DarkMend <gindullin20@mail.ru>
2025-06-07 19:19:49 +0300

    adaptive

--- frontend/src/components/Sidebar/MenuSelect/MenuSelect.tsx
+++ frontend/src/components/Sidebar/MenuSelect/MenuSelect.tsx
@@ -8,6 +8,8 @@ import { IProject } from "../../../interfaces/project.interface";
 import SkeletonItem from "../../SkeletonItem/SkeletonItem";
 import { NavLink } from "react-router-dom";
 import cn from "classnames";
+import DropdownMenuLayout from "../../DropdownMenuLayout/DropdownMenuLayout";
+import DropdownMenuItem from "../../DropdownMenuLayout/DropdownMenuItem";
 
 export interface IMenuSelect {
   name: string;
@@ -33,22 +35,110 @@ export default function MenuSelect({
           classNameContainer={styles.skeletonWrapper}
         />
       ) : (
-        <div className={styles.menuSelectMain}>
-          <div className={styles.menuSelectMainInfo}>
-            <div className={styles.icon}>{icon}</div>
-            <div className={styles.text}>{name}</div>
+        <>
+          <div className={styles.menuSelectMain}>
+            <div className={styles.menuSelectMainInfo}>
+              <div className={styles.icon}>{icon}</div>
+              <div className={styles.text}>{name}</div>
+            </div>
+            <div className={styles.menuSelectMainActions}>
+              <button
+                className={styles.action}
+                onClick={() => openModal(<CreateOrUpdateProjectModal />)}
+              >
+                <PlusIcon />
+              </button>
+            </div>
           </div>
-          <div className={styles.menuSelectMainActions}>
-            <button
-              className={styles.action}
-              onClick={() => openModal(<CreateOrUpdateProjectModal />)}
-            >
-              <PlusIcon />
-            </button>
+          <div className={styles.dropdownMobile}>
+            <DropdownMenuLayout
+              buttonTrigger={
+                <button
+                  className={cn(
+                    styles.menuSelectMain,
+                    styles.menuSelectMainMobile
+                  )}
+                >
+                  <div className={styles.menuSelectMainInfo}>
+                    <div className={styles.icon}>{icon}</div>
+                    <div className={styles.text}>{name}</div>
+                  </div>
+                </button>
+              }
+              className={styles.dropdown}
+              content={
+                <div className={styles.wrapperMobile}>
+                  <DropdownMenuItem>
+                    <div
+                      className={cn(
+                        styles.menuSelectItem,
+                        styles.menuSelectItemMobile
+                      )}
+                    >
+                      <div className={styles.menuSelectMainActions}>
+                        <button
+                          className={styles.action}
+                          onClick={() =>
+                            openModal(<CreateOrUpdateProjectModal />)
+                          }
+                        >
+                          <PlusIcon />
+                        </button>
+                      </div>
+                    </div>
+                  </DropdownMenuItem>
+                  <div className={styles.menuSelectItems}>
+                    {projects?.map((project: IProject) => {
+                      return (
+                        <NavLink
+                          to={`/projects/${project.id}`}
+                          key={project.id}
+                        >
+                          {({ isActive }) => (
+                            <DropdownMenuItem>
+                              <div
+                                className={cn(styles.menuSelectItem, {
+                                  [styles.active]: isActive,
+                                })}
+                              >
+                                <div
+                                  className={cn(
+                                    styles.menuSelectMainInfo,
+                                    styles.menuSelectMainInfoMobile
+                                  )}
+                                >
+                                  <div>
+                                    <div className={styles.icon}>
+                                      {project.icon ? (
+                                        <img
+                                          src={project.icon as string}
+                                          alt={project.name}
+                                        />
+                                      ) : (
+                                        <TeamProjectIcon />
+                                      )}
+                                    </div>
+                                  </div>
+                                  <div className={styles.text}>
+                                    {project.name}
+                                  </div>
+                                </div>
+                              </div>
+                            </DropdownMenuItem>
+                          )}
+                        </NavLink>
+                      );
+                    })}
+                  </div>
+                </div>
+              }
+            />
           </div>
-        </div>
+        </>
       )}
-      <div className={styles.menuSelectItems}>
+      <div
+        className={cn(styles.menuSelectItems, styles.menuSelectItemsDesktop)}
+      >
         {!isFetching &&
           projects?.map((project: IProject) => {
             return (
