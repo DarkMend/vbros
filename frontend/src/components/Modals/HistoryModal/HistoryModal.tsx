b83231326c6c5dda36240ae4ac282a8d6dc43237
DarkMend <gindullin20@mail.ru>
2025-06-21 21:41:27 +0300

    copy text

--- frontend/src/components/Modals/HistoryModal/HistoryModal.tsx
+++ frontend/src/components/Modals/HistoryModal/HistoryModal.tsx
@@ -20,17 +20,19 @@ export default function HistoryModal() {
         {isLoading ? (
           <SkeletonItem count={3} className={styles.skeleton} />
         ) : histories.length > 0 ? (
-          histories?.map((history: IHistory) => (
-            <HistoryItem
-              key={history.id}
-              name={history.projectName}
-              startProject={new Date(history.startProject)}
-              finishProject={
-                history.finishProject ? new Date(history.finishProject) : null
-              }
-              projectIcon={history.projectIcon}
-            />
-          ))
+          histories
+            ?.map((history: IHistory) => (
+              <HistoryItem
+                key={history.id}
+                name={history.projectName}
+                startProject={new Date(history.startProject)}
+                finishProject={
+                  history.finishProject ? new Date(history.finishProject) : null
+                }
+                projectIcon={history.projectIcon}
+              />
+            ))
+            .reverse()
         ) : (
           <div className={styles.text}>У вас пока нет проектов в истории</div>
         )}
--- frontend/src/components/Modals/LinkModal/LinkModal.tsx
+++ frontend/src/components/Modals/LinkModal/LinkModal.tsx
@@ -1,11 +1,41 @@
+import { FileOutput } from "lucide-react";
 import MainInput from "../../MainInput/MainInput";
 import ModalLayout from "../../ModalLayout/ModalLayout";
 import ShareIcon from "./../../../../public/icons/share.svg";
+import styles from "./LinkModal.module.scss";
+import cn from "classnames";
+import { useState } from "react";
 
 export default function LinkModal({ value }: { value: string }) {
+  const [activeText, setActiveText] = useState(false);
+
+  const copyText = () => {
+    navigator.clipboard.writeText(value);
+    setActiveText(true);
+    setTimeout(() => setActiveText(false), 500);
+  };
+
   return (
     <ModalLayout icon={<ShareIcon />} title="Ссылка на проект">
-      <MainInput readOnly value={value} style={{ fontSize: "14px" }} />
+      <div className={styles.wrapper}>
+        <MainInput
+          readOnly
+          value={value}
+          style={{ fontSize: "14px", width: "100%" }}
+        />
+        <div className={styles.button} onClick={copyText}>
+          <div
+            className={cn(styles.show, {
+              [styles.active]: activeText,
+            })}
+          >
+            Скопировано!
+          </div>
+          <div className={styles.icon}>
+            <FileOutput />
+          </div>
+        </div>
+      </div>
     </ModalLayout>
   );
 }
